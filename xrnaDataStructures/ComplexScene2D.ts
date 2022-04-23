8bdc4b8dabba18f1cb198696af60cacb1b139c1f
Caeden <caedenmeade@gatech.edu>
2022-04-23 03:41:46 -0400

    Initial commit

--- xrnaDataStructures/ComplexScene2D.ts
+++
@@ -1,7 +0,0 @@
-import { ComplexScene } from "./ComplexScene"
-
-export class ComplexScene2D extends ComplexScene {
-    public constructor(name : string, author : string, psScale=1.0, landscapeModeFlag = false) {
-        super(name, author);
-    }
-}
