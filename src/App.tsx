003b18ba0f523e5f24a5558ff4473432a4759e78
Caeden <caedenmeade@gatech.edu>
2022-04-24 01:13:48 -0400

    Completed skeletons of the import/export tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import { useState } from 'react'
+import { useState, createRef, useEffect } from 'react'
 
 type ButtonData = {
   highlightColor : string
 };
+
+interface FileReader {
+  (inputFileContent : string) : void;
+}
+
+interface FileGenerator {
+  () : string;
+}
 
 enum Tab {
   IMPORT_EXPORT = "Import/Export",
@@ -27,55 +35,77 @@ function App() {
     }
   };
   const [currentTab, setCurrentTab] = useState<Tab>(Tab.IMPORT_EXPORT);
-  
-  const buttons = [];
-  var buttonIndex = 0;
-  for (const [tabName, buttonDatum] of Object.entries(buttonData)) {
-    const style = {
-      border : "groove gray",
-      color : currentTab == tabName ? "white" : "black",
-      backgroundColor : currentTab == tabName ? buttonDatum.highlightColor : "white"
-    };
-    buttons.push(<button style={style} key={buttonIndex} onClick={() => setCurrentTab(tabName as Tab)}>{tabName}</button>);
-    buttonIndex++;
-  }
   const [showTabReminderFlag, setShowTabReminderFlag] = useState<boolean>(true);
-  const boundingDivStyle = {
-    border : showTabReminderFlag ? "ridge " + (buttonData[currentTab] as ButtonData).highlightColor : "none" ,
-    color : "white"
-  };
-  const toolsDivStyle = {
-    backgroundColor : "rgb(54, 64, 79)",
-    width : "100"
-  };
-  const importExportDivStyle = {
-    display : currentTab == Tab.IMPORT_EXPORT ? "block" : "none"
-  };
-  const editDivStyle = {
-    display : currentTab == Tab.EDIT ? "block" : "none"
-  };
-  const annotateDivStyle = {
-    display : currentTab == Tab.ANNOTATE ? "block" : "none"
-  };
-  const settingsDivStyle = {
-    display : currentTab == Tab.SETTINGS ? "block" : "none"
-  };
   const [outputFileName, setOutputFileName] = useState<string>("");
-  const [syncOutputFileNameToInputFileNameFlag, setSyncOutputFileNameToInputFileNameFlag] = useState<boolean>(true);
-  return (
-    <div style={boundingDivStyle}>
-      <div style={toolsDivStyle}>
-        {buttons}
-        <div style={importExportDivStyle}>
+  const [copyInputFileNameToOutputFileNameFlag, setCopyInputFileNameToOutputFileNameFlag] = useState<boolean>(true);
+  const [copyInputFileExtensionToOutputFileExtensionFlag, setCopyInputFileExtensionToOutputFileExtensionFlag] = useState<boolean>(true);
+  const [outputFileExtension, setOutputFileExtension] = useState<string>("");
+  const inputFileReaders : Record<string, FileReader> = {
+    "xrna" : (_ : string) => {
+      console.log("Reading XRNA input file.");
+    },
+    "str" : (_ : string) => {
+      console.log("Reading str input file.");
+    }
+  };
+  const [downloadAnchorHref, setDownloadAnchorHref] = useState<string>();
+  const outputFileGenerators : Record<string, FileGenerator> = {
+    "json" : () => "This is a JSON file.",
+    "xrna" : () => "This is an XRNA file.",
+    "svg" : () => "This is an SVG file."
+  };
+  const outputFileExtensionSelectRef = createRef<HTMLSelectElement>();
+  const downloadAnchorRef = createRef<HTMLAnchorElement>();
+  useEffect(() => {
+    // Upon load:
+    // * Initialize the output-file select.
+    (outputFileExtensionSelectRef.current as HTMLSelectElement).selectedIndex = -1;
+  }, []);
+  useEffect(() => {
+    (downloadAnchorRef.current as HTMLAnchorElement).click();
+  }, [downloadAnchorHref]);
+  return ( 
+    <div style={{
+      border : showTabReminderFlag ? "ridge " + (buttonData[currentTab] as ButtonData).highlightColor : "none" ,
+      color : "white"
+    }}>
+      <div style={{
+        backgroundColor : "rgb(54, 64, 79)",
+        width : "100"
+      }}>
+        {Object.entries(buttonData).map(([tabName, buttonDatum]) => {
+          return <button style={{
+            border : "groove gray",
+            color : currentTab == tabName ? "white" : "black",
+            backgroundColor : currentTab == tabName ? buttonDatum.highlightColor : "white"
+          }} key={tabName} onClick={() => setCurrentTab(tabName as Tab)}>{tabName}</button>;
+        })}
+        <div style={{
+          display : currentTab == Tab.IMPORT_EXPORT ? "block" : "none"
+        }}>
           <label>
             Upload an input file:&nbsp;
-            <input type="file" onChange={event => {
-              if (syncOutputFileNameToInputFileNameFlag) {
-                let files = event.target.files;
-                if (files && files.length > 0) {
-                  let newValue = (files[0] as File).name;
-                  setOutputFileName(newValue.substring(0, newValue.lastIndexOf(".")));
+            <input type="file" accept={Object.keys(inputFileReaders).map(inputFileExtension => "." + inputFileExtension).join(",")} onChange={event => {
+              let files = event.target.files;
+              if (files && files.length > 0) {
+                let inputFileName = (files[0] as File).name;
+                let lastIndexOfPeriod = inputFileName.lastIndexOf(".");
+                if (copyInputFileNameToOutputFileNameFlag) {
+                  setOutputFileName(inputFileName.substring(0, lastIndexOfPeriod));
                 }
+                let outputFileExtension = inputFileName.substring(lastIndexOfPeriod + 1);
+                if (copyInputFileExtensionToOutputFileExtensionFlag) {
+                  let newSelectedIndex = Array.from((outputFileExtensionSelectRef.current as HTMLSelectElement).children).findIndex(child => child.getAttribute("value") === outputFileExtension);
+                  (outputFileExtensionSelectRef.current as HTMLSelectElement).selectedIndex = newSelectedIndex;
+                  setOutputFileExtension(newSelectedIndex == -1 ? "" : outputFileExtension);
+                }
+                let reader = new FileReader();
+                reader.addEventListener("load", event => {
+                  // Read the content of the input file.
+                  let inputFileContent = (event.target as globalThis.FileReader).result as string;
+                  (inputFileReaders[outputFileExtension] as FileReader)(inputFileContent);
+                });
+                reader.readAsText(files[0] as File);
               }
             }}></input>
           </label>
@@ -84,24 +114,43 @@ function App() {
             Create a downloadable output file:&nbsp;
             <input type="text" value={outputFileName} onChange={event => setOutputFileName(event.target.value)}></input>
           </label>
-          <select>
+          <select onChange={event => setOutputFileExtension(event.target.value)} ref={outputFileExtensionSelectRef}>
+            {Object.entries(outputFileGenerators).map(([fileExtension, ]) => {
+              return <option key={fileExtension} value={fileExtension}>{"." + fileExtension}</option>
+            })}
           </select>
+          <a href={downloadAnchorHref} download={outputFileName + "." + outputFileExtension} style={{
+            display : "none"
+          }} ref={downloadAnchorRef}></a>
+          <button onClick={() => {
+            setDownloadAnchorHref(`data:text/plain;charset=utf-8,${encodeURIComponent((outputFileGenerators[outputFileExtension] as FileGenerator)())}`);
+          }} disabled={!outputFileName || !outputFileExtension}>Download</button>
         </div>
-        <div style={editDivStyle}>
+        <div style={{
+          display : currentTab == Tab.EDIT ? "block" : "none"
+        }}>
 
         </div>
-        <div style={annotateDivStyle}>
+        <div style={{
+          display : currentTab == Tab.ANNOTATE ? "block" : "none"
+        }}>
 
         </div>
-        <div style={settingsDivStyle}>
+        <div style={{
+          display : currentTab == Tab.SETTINGS ? "block" : "none"
+        }}>
           <label>
             Show tab reminder: 
             <input type="checkbox" onChange={() => setShowTabReminderFlag(!showTabReminderFlag)} checked={showTabReminderFlag}></input>
           </label>
           <br />
           <label>
-            Sync output file name to input file name:
-            <input type="checkbox" onChange={() => setSyncOutputFileNameToInputFileNameFlag(!syncOutputFileNameToInputFileNameFlag)} checked={syncOutputFileNameToInputFileNameFlag}></input>
+            Copy input-file name and extension to output-file name&nbsp;
+            <input type="checkbox" onChange={() => setCopyInputFileNameToOutputFileNameFlag(!copyInputFileNameToOutputFileNameFlag)} checked={copyInputFileNameToOutputFileNameFlag}></input>
+          </label>
+          <label>
+            &nbsp;and extension
+            <input type="checkbox" onChange={() => setCopyInputFileExtensionToOutputFileExtensionFlag(!copyInputFileExtensionToOutputFileExtensionFlag)} checked={copyInputFileExtensionToOutputFileExtensionFlag}></input>
           </label>
         </div>
       </div>
